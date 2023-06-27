a40ccb287ef10f67e6d28c28ca710e043a4ab1af
Luna Goodnight <luna.lee@nextfortune.tw>
2023-06-27 09:12:53 +0800

    prepare news component

--- app/DashboardChartSection.tsx
+++ app/DashboardChartSection.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import { AreaChart } from "@/app/AreaChart";
+
 import { NumberDataSection } from "@/app/NumberDataSection";
 import { RevenueBarChart } from "@/app/RevenueBarChart";
 import { VisitedDonut } from "@/app/VisitedDonut";
 
-export const DashboardChartSection = () => {
+export const DashboardChartSection = async () => {
   return (
     <div className="flex w-full flex-wrap">
       <NumberDataSection />
--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: "export" };
+const nextConfig = {
+  output: "export",
+  experimental: {
+    serverActions: true,
+  },
+  env: {
+    NEWS_API_KEY: process.env.NEWS_API_KEY,
+  },
+};
 
 module.exports = nextConfig;
