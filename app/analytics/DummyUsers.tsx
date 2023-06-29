f6950440afad378cc54f4ac7f4168fb4de81175f
Luna Goodnight <luna.lee@nextfortune.tw>
2023-06-29 14:48:05 +0800

    add mock user list

--- app/analytics/DummyUsers.tsx
+++ app/analytics/DummyUsers.tsx
@@ -13,7 +13,7 @@ async function getData() {
 
 export const DummyUsers = async () => {
   const data = await getData();
-  console.log({ data });
+
   return (
     <div className="w-2/6 bg-white p-4 rounded text-left shadow-md relative dark:bg-neutral-600 dark:text-gray-300">
       <h4 className="text-sm text-gray-400 pb-2">Top Sales</h4>
