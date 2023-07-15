87a2d154a34df52ca3f99d34b3b094206b8860ad
Luna Goodnight <luna.lee@nextfortune.tw>
2023-07-15 21:45:42 +0800

    add user panel

--- app/UserPanel.tsx
+++ app/UserPanel.tsx
@@ -3,11 +3,19 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const UserPanel = () => {
   return (
-    <div className="group">
-      <FontAwesomeIcon className="text-[#adb4d2]-500" icon={faCircleUser} />
-      <div className="hidden group-hover:block w-6 h-8 bg-emerald-300 absolute top-3 right-0">
-        adfadfasfd{" "}
+    <div className="group relative">
+      <div className="p-2 cursor-pointer">
+        <FontAwesomeIcon className="text-[#adb4d2]-500" icon={faCircleUser} />
       </div>
+      <ul className="hidden group-hover:block bg-gray-50 dark:bg-neutral-800 absolute top-11 right-0 rounded z-10 min-w-[180px]">
+        <li className="p-2 border-b-2 border-b-gray-300 dark:border-b-gray-600 w-full">
+          Profile
+        </li>
+        <li className="p-2 border-b-2 border-b-gray-300 dark:border-b-gray-600 w-full">
+          Settings
+        </li>
+        <li className="p-2 w-full">Logout</li>
+      </ul>
     </div>
   );
 };
